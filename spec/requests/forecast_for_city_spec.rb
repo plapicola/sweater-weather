b674fac4619459032fa80fc1f1c09a96a41203f2
plapicola <p.lapicola@gmail.com>
2019-04-12 20:54:35 -0600

    Adds request spec for forecast endpoint

---
+++ spec/requests/forecast_for_city_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast API', type: :request do
+  context 'as a visitor' do
+    it "I can request the current forecast for a city, state" do
+      get '/api/v1/forecast?denver,co'
+
+      weather_info = JSON.parse(response.body)[:data]
+
+      expect(weather_info).to have_key :id
+      expect(weather_info).to have_key :attributes
+      expect(weather_info[:attributes]).to have_key :name
+      expect(weather_info[:attributes][:name]).to eq 'Denver, CO'
+      expect(weather_info[:attributes]).to have_key :current
+      expect(weather_info[:attributes]).to have_key :hourly_forecast
+      expect(weather_info[:attributes]).to have_key :daily_forecast
+
+      current_forecast = weather_info[:attributes][:current_weather]
+
+      expect(current_weather).to have_key :current_temperature
+      expect(current_weather).to have_key :perceived_temperature
+      expect(current_weather).to have_key :max_temperature
+      expect(current_weather).to have_key :min_temperature
+      expect(current_weather).to have_key :current_weather
+      expect(current_weather).to have_key :humidity
+      expect(current_weather).to have_key :visibility
+      expect(current_weather).to have_key :uv_index
+      expect(current_weather).to have_key :current_description
+      expect(current_weather).to have_key :future_description
+
+      hourly_forecast = weather_info[:attributes][:hourly_forecast]
+
+      expect(hourly_forecast).to be_a Array
+      expect(hourly_forecast[0]).to have_key :hour
+      expect(hourly_forecast[0]).to have_key :temperature
+      expect(hourly_forecast[0]).to have_key :weather
+
+      daily_forecast = weather_info[:attributes][:daily_forecast]
+
+      expect(daily_forecast).to be_a Array
+      expect(daily_forecast[0]).to have_key :day
+      expect(daily_forecast[0]).to have_key :weather
+      expect(daily_forecast[0]).to have_key :percipitation_chance
+      expect(daily_forecast[0]).to have_key :max_temperature
+      expect(daily_forecast[0]).to have_key :min_temperature
+    end
+  end
+end
