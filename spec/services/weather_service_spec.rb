75f5c4ca928980fcd4b96e13663cffa601ad3a44
plapicola <p.lapicola@gmail.com>
2019-04-14 10:37:25 -0600

    Adds test for weather service

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,16 +1,70 @@
 reqire 'rails_helper'
 
 RSpec.describe WeatherService, type: :service do
-  it 'exists' do
-    service = WeatherService.new(coordinates)
+  before :each do
+    @coordinates = {
+      lat: 39.7392358,
+      long: -104.990251
+    }
 
+    @service = WeatherService.new(@coordinates)
+  end
+  it 'exists' do
     expect(service).to be_a WeatherService
   end
 
   describe 'instance methods' do
     describe 'get_forecast' do
       it 'requests the forecast for the services coordinates' do
-        
+        results = @service.get_forecast
+
+        expect(results).to be_a Hash
+        expect(results).to have_key :currently
+        expect(results).to have_key :hourly
+        expect(results).to have_ley :daily
+
+        current_results = results[:currently]
+
+        expect(current_results).to have_key :time
+        expect(current_results).to have_key :precipIntensity
+        expect(current_results).to have_key :precipProbability
+        expect(current_results).to have_key :temperature
+        expect(current_results).to have_key :apparentTemperature
+        expect(current_results).to have_key :humidity
+        expect(current_results).to have_key :uvIndex
+        expect(current_results).to have_key :visibility
+        expect(current_results).to have_key :ozone
+
+        hourly_results = results[:hourly]
+
+        expect(hourly_results).to be_a Hash
+        expect(hourly_results).to have_key :data
+        expect(hourly_results[:data]).to be_a Array
+        expect(hourly_results[:data][0]).to have_key :time
+        expect(hourly_results[:data][0]).to have_key :precipProbability
+        expect(hourly_results[:data][0]).to have_key :temperature
+        expect(hourly_results[:data][0]).to have_key :apparentTemperature
+        expect(hourly_results[:data][0]).to have_key :humidity
+        expect(hourly_results[:data][0]).to have_key :uvIndex
+        expect(hourly_results[:data][0]).to have_key :visibility
+        expect(hourly_results[:data][0]).to have_key :ozone
+
+        daily_results = results[:daily]
+
+        expect(daily_results).to be_a Hash
+        expect(daily_results).to have_key :data
+        expect(daily_results[:data]).to be_a Array
+        expect(daily_results[:data][0]).to have_key :time
+        expect(daily_results[:data][0]).to have_key :precipProbability
+        expect(daily_results[:data][0]).to have_key :temperature
+        expect(daily_results[:data][0]).to have_key :apparentTemperature
+        expect(daily_results[:data][0]).to have_key :humidity
+        expect(daily_results[:data][0]).to have_key :uvIndex
+        expect(daily_results[:data][0]).to have_key :visibility
+        expect(daily_results[:data][0]).to have_key :ozone
+        expect(daily_results[:data][0]).to have_key :temperatureHigh
+        expect(daily_results[:data][0]).to have_key :temperatureLow
+
       end
     end
   end
