79754310990668e97328c1071e3884154e8aefb1
plapicola <p.lapicola@gmail.com>
2019-04-14 17:38:36 -0600

    Adds endpoint test for backgrounds

---
+++ spec/requests/photo_for_location_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Backgrounds API' do
+  context 'as a visitor' do
+    it 'I can request a background image for a location' do
+      get '/api/v1/backgrounds?location=denver,co'
+
+      image = JSON.parse(response.body, symbolize_names: true)[:data][0]
+
+      expect(image).to be_a Hash
+      expect(image).to have_key :attributes
+      expect(image[:attributes]).to have_key :url
+    end
+  end
+end
