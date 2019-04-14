827d71ae9532ba3efa3366aa2ea975a78fb46871
plapicola <p.lapicola@gmail.com>
2019-04-13 23:03:44 -0600

    Adds geolocation service

---
+++ app/services/geocode_service.rb
@@ -0,0 +1,29 @@
+class GeocodeService
+  def initialize(city, state)
+    @city = city.gsub(" ", "+")
+    @state = state
+  end
+
+  def get_coordinates
+    parse(request_coordinates)[:results][0][:geometry][:location]
+  end
+
+  private
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def request_coordinates
+    conn.get do |req|
+      req.params[:address] = "#{@city},+#{@state}"
+    end
+  end
+
+  def conn
+    Faraday.new(url: 'https://maps.googleapis.com/maps/api/geocode/json') do |f|
+      f.params[:key] = ENV['GOOGLE_GEOCODE_KEY']
+      f.adapter Faraday.default_adapter
+    end
+  end
+end
