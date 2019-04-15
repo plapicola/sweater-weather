88c31b1cbdd3dcf8190119ba8dfb6e42a089139c
plapicola <p.lapicola@gmail.com>
2019-04-15 09:54:01 -0600

    Adds antipode service spec

---
+++ spec/services/antipode_service_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe AntipodeService, type: :service do
+  it 'exists' do
+    service = AntipodeService.new
+
+    expect(service).to be_a AntipodeService
+  end
+
+  describe 'instance_methods' do
+    describe '.get_antipode' do
+      it 'returns the antipode coordinates for a provided location' do
+        service = AntipodeService.new
+
+        search_location = {
+          lat: 22.561968,
+          lng: 114.4294999
+        }
+
+        antipode = service.get_antipode(search_location)
+
+        expect(antipode).to have_key :lat
+        expect(antipode).to have_key :lng
+        expect(antipode[:lat]).to eq(-24.1843397)
+        expect(antipode[:lng]).to eq(-65.302177)
+      end
+    end
+  end
+
+end
