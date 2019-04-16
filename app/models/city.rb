fa4dc4ec85506a2c7dbfb7e7bd3387873a301899
plapicola <p.lapicola@gmail.com>
2019-04-16 04:54:55 -0600

    Adds city model validations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,20 @@
 class City < ApplicationRecord
+  has_many :favorites
+  has_many :users, through: :favorites
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates :latitude,
+    presence: true,
+    numericality: {
+      greater_than_or_equal_to: -180,
+      less_than_or_equal_to: 180
+  }
+
+  validates :longitude,
+    presence: true,
+    numericality: {
+      greater_than_or_equal_to: -180,
+      less_than_or_equal_to: 180
+  }
 end
