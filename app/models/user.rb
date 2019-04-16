a5a2cd543aab9400143759317a0a13a5fee38719
plapicola <p.lapicola@gmail.com>
2019-04-16 04:57:25 -0600

    Adds relationships to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :favorites
+  has_many :cities, through: :favorites
+
   validates_presence_of :email
   validates_uniqueness_of :email
   validates_confirmation_of :password
