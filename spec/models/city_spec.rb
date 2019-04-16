6c88982d659b556c2d01c14637d02bfebf33a677
plapicola <p.lapicola@gmail.com>
2019-04-16 05:21:42 -0600

    Adds coordinates test to city model

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -20,4 +20,19 @@ RSpec.describe City, type: :model do
     it {should have_many :favorites}
     it {should have_many(:users).through(:favorites)}
   end
+
+  describe 'instance methods' do
+    describe '.coordinates' do
+      it 'returns a hash of coordinates formatted for other services' do
+        denver = City.create(name: "Denver, CO", latitude: 39.7392358, longitude: -104.990251)
+
+        expected = {
+          lat: 39.7392358,
+          lng: -104.990251
+        }
+
+        expect(denver.coordinates).to eq(expected)
+      end
+    end
+  end
 end
