433ed7af0d36919d68a43f6293a2b9a20bf968e5
plapicola <p.lapicola@gmail.com>
2019-04-16 18:12:41 -0600

    Adds caching and cache testing

--- spec/requests/photo_for_location_spec.rb
+++ spec/requests/photo_for_location_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Backgrounds API' do
   context 'as a visitor' do
+    before :each do
+      Rails.cache.clear
+    end
     it 'I can request a background image for a location' do
       get '/api/v1/backgrounds?location=denver,co'
 
@@ -10,6 +13,13 @@ RSpec.describe 'Backgrounds API' do
       expect(image).to be_a Hash
       expect(image).to have_key :attributes
       expect(image[:attributes]).to have_key :url
+    end
+
+    it 'subsequent requests for a location are cached' do
+      expect(PhotoService).to receive(:new).once.and_call_original
+
+      get '/api/v1/backgrounds?location=denver,co'
+      get '/api/v1/backgrounds?location=denver,co'
     end
   end
 end
